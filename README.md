6dcbd3e3693a087113cd9a3e9de55af4631cc2f6
euloge clovis kenne pagui <kenne@stat.unipd.it>
2018-07-12 13:05:40 +0200

    upload files

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# coxphkss
+# Welcome to the ongoing work of the library coxphkss
+
+# [library coxphkss]
+
+* * *
+
+## Set up
+Here are the steps to follow in order to obtain the  median bias 
+reduced  regression parameter estimates of the Cox model based on the median adjusted score function proposed in Kenne Pagui et al. (2017):
+
+**1-** Download the repository coxphkss on your own computer
+
+**2-** Set the working directory  where there is the coxphkss repository
+
+
+**3-**  Open the file examples.R for the numerical examples on Cox regression
+
+* * *
+
+## Authors
+
+The coxphkss implementation has been written by **Euloge Clovis Kenne Pagui** (kenne@stat.unipd.it), from the University of Padova-Italy and **Enrico Colosimo** (enricoc@est.ufmg.br),  from the Federal University of Minas Gerais, Belo Horizonte-Brazil.
+
+## References
+
+Kenne Pagui, E. C., Salvan, A. and Sartori, N. (2017). Median bias reduction of maximum likelihood estimates. *Biometrika*, 104 923-938.
---
+++ example.R
@@ -0,0 +1,49 @@
+#####################################################
+## median bias reduction for Cox regression      ####
+#####################################################
+
+source("coxRegressionMedianBR.R")
+
+dataCens <- data.frame(time=c(6,7,1,2,3,4,5), 
+                       status=c(1,1,1,0,1,1,0), 
+                       x=c(0,2,1,1,1,0,0), 
+                       sex=c(0,0,0,0,1,1,1)) 
+
+dataNoCens <- data.frame(time=c(6,7,1,2,3,4,5), 
+                         status=c(1,1,1,1,1,1,1), 
+                         x=c(0,2,1,1,1,0,0), 
+                         sex=c(0,0,0,0,1,1,1)) 
+
+
+
+####################################
+##  maximum likelihood method fit ##
+####################################
+
+ml<-coxph(Surv(time, status) ~ x + sex, data=dataCens ) 
+summary(ml)
+ml2<-coxph(Surv(time, status) ~ x + sex, data=dataNoCens ) 
+summary(ml2)
+
+
+####################################
+##  mean bias reduced fit (Firth) ##
+####################################
+
+meanBR<-coxphf(Surv(time, status) ~ x + sex, data=dataCens ) 
+summary(meanBR)
+meanBR2<-coxphf(Surv(time, status) ~ x + sex, data=dataNoCens ) 
+summary(meanBR2)
+
+######################################
+##  median bias reduced fit (Firth) ##
+######################################
+
+medianBR <- coxphkss(beta=c(0,0),data=dataCens,type = "AS_median")
+medianBR
+medianBR2 <- coxphkss(beta=c(0,0),data=dataNoCens,type = "AS_median")
+medianBR2
+
+
+
+
